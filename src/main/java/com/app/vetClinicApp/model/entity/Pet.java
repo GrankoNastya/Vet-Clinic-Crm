925d37ccc903dfe74dbfe5fd2658ae40c4c1df3c
GrankoNastya <160241052+GrankoNastya@users.noreply.github.com>
2024-12-20 18:24:03 +0500

    Update Pet.java

--- src/main/java/com/app/vetClinicApp/model/entity/Pet.java
+++ src/main/java/com/app/vetClinicApp/model/entity/Pet.java
@@ -13,13 +13,13 @@ public class Pet extends BaseEntity {
     private String name;
     private String breed;
     @Column(name = "species")
-    private String species;   //what type of pet (cat,dog,bird etc..)
+    private String species;   //Тип животного (кот, собака, птица и т.д.)
     private int age;
-    private String statement;  //description about pet health
+    private String statement;  //Описание здоровья питомца
     private String gender;
 
 
     @ManyToOne(fetch=FetchType.LAZY ,cascade = CascadeType.PERSIST)
     @JoinColumn(updatable = false)
-    private PetOwner petOwner;  //a pet must have just one owner
+    private PetOwner petOwner;  //У питомца может быть только один владелец
 }
